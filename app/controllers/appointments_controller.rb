7b5e46fd921d7ea4108a7aabb346c723728caa91
Theresa Palid <tpalid1@gmail.com>
2016-08-13 17:55:19 +0000

    Refactored appointment controller and views

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,25 +1,29 @@
 class AppointmentsController < ApplicationController
-    def index
-        @team = Team.find(params[:id])
-        @appointments = @team.appointments
-    end
+    # def index
+    #     @team = Team.find(params[:id])
+    #     @appointments = @team.appointments
+    # end
     
     def show
+        @patient = Patient.find(params[:patient_id])
+        @team = @patient.team
         @appointment = Appointment.find(params[:id])
     end
     
     def new
-        @team = Team.find(params[:team_id])
+        @patient = Patient.find(params[:patient_id])
+        @team = @patient.team
         @appointment = Appointment.new
     end
     
     def create
         @appointment = Appointment.new(appointment_params)
-        @team = Team.find(params[:team_id])
+        @patient = Patient.find(params[:patient_id])
+        @team = @patient.team
         @appointment.team = @team
         if @appointment.save
             flash[:notice] = "#{@appointment.team.patient}'s appointment with #{@appointment.doctor} was saved succesfully."
-            redirect_to [@team]
+            redirect_to [@patient, @team]
         else
             flash[:error] = "There was an error saving the appointment. Please try again."
             render :new
@@ -27,16 +31,20 @@ class AppointmentsController < ApplicationController
     end
     
     def edit
+        @patient = Patient.find(params[:patient_id])
+        @team = @patient.team
         @appointment = Appointment.find(params[:id])
     end
     
     def update
-        @appointment = Appointment.find(params[:appointment_id])
+        @patient = Patient.find(params[:patient_id])
+        @team = @patient.team
+        @appointment = Appointment.find(params[:id])
         if @appointment.update_attributes(appointment_params)
-            flash[:notice] = "#{@appointment.team.patient}'s appointment has been successfully updated."
-            redirect_to @appointment.team
+            flash[:notice] = "#{@appointment.team.patient.name}'s appointment has been successfully updated."
+            redirect_to patient_team_path
         else
-            flash[:notice] = "There was an error updating #{@appointment.team.patient}'s appointment. Please try again."
+            flash[:notice] = "There was an error updating #{@appointment.team.patient.name}'s appointment. Please try again."
             render :edit
         end
     end
@@ -45,7 +53,7 @@ class AppointmentsController < ApplicationController
         @appointment = Appointment.find(params[:id])
         if @appointment.destroy
             flash[:notice] = "#{@appointment.team.patient}'s appointment was deleted."
-            redirect_to @appointment.team
+            redirect_to patient_team_path
         else
             flash[:error] = "Error deleting appointment. Please try again."
             render :show
